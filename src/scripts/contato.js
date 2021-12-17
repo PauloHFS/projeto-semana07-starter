8857c1d93b65b5a2bbf2ad395245085aea28501c
Paulo Hernane <paulohernane10@gmail.com>
2021-12-17 16:54:47 -0300

    feat(form): added form submit event

--- src/scripts/contato.js
+++ src/scripts/contato.js
@@ -24,4 +24,22 @@ window.onload = () => {
 
     mobile_navbar.style.display = 'none';
   });
+
+  const form = document.querySelector('.contato-form');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const [nomeInput, emailInput, mensageInput] = event.target;
+
+    const body = {
+      [nomeInput.name]: nomeInput.value,
+      [emailInput.name]: emailInput.value,
+      [mensageInput.name]: mensageInput.value,
+    };
+
+    alert(JSON.stringify(body));
+
+    location.href = './index.js';
+  });
 };
